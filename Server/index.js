bd5e8748192f0c4a4b8c64c778131bb1a4af90bc
SaiAIE <ssai@aienterprise.com>
2025-02-12 18:03:34 +0530

    Cors Policy

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,11 @@ dotenv.config()
 connectDB()
 
 const app = express()
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin: 'https://user-crud-r6qj.onrender.com'
+  }));
+  
 app.use(express.json())
 app.use("/api/users",userRoutes)
 
